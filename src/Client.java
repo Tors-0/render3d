69db9c37763bcfabe1e33e4efd2a7f87ad6ecbef
rae <raej0000@gmail.com>
2023-09-11 09:38:44 -0700

    optimize imports

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 public class Client {
     public static void main(String[] args) {
         new Cube(-64,-64,-64,128);
-        new Pyramid(5);
+        new Pyramid(50);
         Render.start();
     }
 }
